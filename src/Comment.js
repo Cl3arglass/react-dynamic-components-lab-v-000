6be88dd9865e348b0fe877024547e7d85d0ea01e
Holden Altaffer <haltaffer@gmail.com>
2019-03-26 02:09:08 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,11 +1,12 @@
 // add Comment component here
 import React, { Component } from 'react'
+import BlogPost from "./BlogPost.js";
 
 class Comment extends React.Component {
   render() {
     return (
       <div>
-        {this.props.articleText}
+        <BlogPost />
       </div>
     )
   }
