5290f0c8463fadd922fb4096a1ee04ec012ea2ee
Holden Altaffer <haltaffer@gmail.com>
2019-03-26 02:04:14 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 // add Comment component here
+import React, { Component } from 'react'
+
+class BlogContent extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.articleText}
+      </div>
+    )
+  }
+}
