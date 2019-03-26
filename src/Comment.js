2de107f9e87005c110285db58f07002fe34ff4d0
Holden Altaffer <haltaffer@gmail.com>
2019-03-26 02:09:20 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,5 +1,5 @@
 // add Comment component here
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import BlogPost from "./BlogPost.js";
 
 class Comment extends React.Component {
